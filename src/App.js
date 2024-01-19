72dcaadec7ea5946e78012d121da668af6874dcd
Shiva Maharjan <shivamaharjan@Shivas-MacBook-Air.local>
2024-01-19 20:29:19 +1100

    add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <div>
+      <h2>Porfolio</h2>
+
+    </div>
+  );
+}
+
+export default App;
